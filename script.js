13897190f2c9e731ae4f6e108b15abb1dc1726b3
Lilian-Moraes <lilianfranca@hotmail.com>
2025-02-26 12:32:13 -0300

    Atualizar código

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function enviarDados() {
-  let nome = document.getElementById("nome").value;
-  let idade = parseInt(document.getElementById("idade").value);
+  // ... (código para obter nome, idade e linguagem) ...
+  let gosta = parseInt(document.getElementById("gosta").value);
   let linguagem = document.getElementById("linguagem").value;
   let resultado = document.getElementById("resultado");
 
@@ -8,5 +8,10 @@ function enviarDados() {
     resultado.innerHTML = "Idade inválida. Por favor, insira um número inteiro positivo.";
   } else {
     resultado.innerHTML = `Olá, ${nome}! Você tem ${idade} anos e está estudando ${linguagem}.`;
+    if (gosta === 1) {
+      resultado.innerHTML += "<br>Muito bom! Continue estudando e você terá muito sucesso.";
+    } else {
+      resultado.innerHTML += "<br>Ahh que pena... Já tentou aprender outras linguagens?";
+    }
   }
 }
